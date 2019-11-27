e8b94ecb3315e6d3cfb5376538affbf8eb576e5b
lzc <443266630@qq.com>
2019-11-27 09:19:14 +0800

    合并代码

--- app/pages/home/moments/index.ts
+++ app/pages/home/moments/index.ts
@@ -1,5 +1,11 @@
 Page({
-  data: {},
+  data: {
+    imageList: [
+      {
+        photo: 'https://lvcui-image.oss-cn-shanghai.aliyuncs.com/static/temp/supremacy_buy/1911/06/2e043e8b0b3eee13ba184e550ab81801.jpg!750'
+      }
+    ]
+  },
   onLoad() {
     console.log('onLoad')
   },
