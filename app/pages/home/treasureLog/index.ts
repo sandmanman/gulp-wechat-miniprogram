42e71dbc8373950fc62463b94bbe6f4b3bc0e578
lzc <443266630@qq.com>
2019-12-02 16:50:24 +0800

    对接玩物日志页面

--- app/pages/home/treasureLog/index.ts
+++ app/pages/home/treasureLog/index.ts
@@ -10,31 +10,37 @@ Page({
     index: 0,
     selected: 0,
     showlabel: true,
-    page: 1
+    pageNumber: 1,
+    canLoadNextPage: false
   },
-  async onLoad(query: Record<string, string | undefined>) {
-    const { id = '' } = query
-    this.setData({ code: id })
+  async onLoad() {
     if (!app.globalData.userInfo.user_code) {
       await app.userLogin()
     }
-    this.getSelsectLogList()
-    this.getTreasureDataList()
+    await this.getSelsectLogList()
+    await this.getTreasureDataList(this.data.titleList[0].id, this.data.pageNumber)
+  },
+  async onReachBottom() {
+    if (this.data.canLoadNextPage) {
+      return false
+    }
+    wx.showLoading({ title: '数据加载中', mask: true })
+    this.data.pageNumber += 1
+    await this.getTreasureDataList(this.data.wiki_id, this.data.pageNumber)
   },
   titleClick({
     currentTarget: {
       dataset = {
         index: 0,
-        id: 1
+        id: 0
       }
     }
   }) {
     const { index, id } = dataset
     this.setData({
-      selected: index,
-      wiki_id: id
+      selected: index
     })
-    this.getTreasureDataList()
+    this.getTreasureDataList(id, 1)
   },
   openbtn() {
     this.setData({
@@ -46,14 +52,24 @@ Page({
       showlabel: true
     })
   },
-  async getTreasureDataList(type: 1 | 2 | 3 | 4, wiki_id = 1, page = 1) {
+  async getTreasureDataList(wiki_id: number, page = 1) {
+    wx.showLoading({ title: '数据加载中', mask: true })
     const params = getSignature({
       c_p: app.globalData.c_p,
       code: this.data.code,
       wiki_id,
-      type: 2,
+      type: 2 as 1 | 2 | 3 | 4,
       page
     })
+    // try {
+    //   const { obj } = await api.getDynamicList(params)
+    //   const listData = this.data.articleList
+    //   listData = listData.concat(obj.list)
+    //   const nomore = obj.current_page === obj.last_page
+    //   this.setData({ articleList: listData, nomore }, wx.hideLoading)
+    // } catch (error) {
+    //   console.error(error)
+    // }
     const data = await api.getDynamicList(params)
     console.log(data)
     this.setData({ articleList: data.obj.list })
@@ -66,7 +82,6 @@ Page({
       page: 1
     })
     const data = await api.selsectArticleList(params)
-    console.log(data)
     this.setData({ titleList: data.obj })
   }
 })
